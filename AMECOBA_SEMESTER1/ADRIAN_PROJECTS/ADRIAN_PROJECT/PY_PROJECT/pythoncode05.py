5b4ee6f433376eb60b7fd5922a373eecdd9b9fae
adrian-zulnedi <z.adrian@arkth.co.jp>
2020-09-23 17:15:53 +0900

    initial commit

--- AMECOBA_SEMESTER1/ADRIAN_PROJECTS/ADRIAN_PROJECT/PY_PROJECT/pythoncode05.py
+++ AMECOBA_SEMESTER1/ADRIAN_PROJECTS/ADRIAN_PROJECT/PY_PROJECT/pythoncode05.py
@@ -1,1 +1,22 @@
-my_dict = dict()
+persons = {"person_num1" : "1",
+        "person_num2" : "2",
+        "person_num3" : "3",
+        "person_num4" : "4"
+}
+
+
+for person in persons:
+    print(person)
+
+
+fruits = {"apple" : "200",
+        "banana" : "100",
+        "orange" : "300"}
+
+
+del fruits["banana"]  #バリューの方を指定したらKEYERRORが起こるから気をつけること
+
+
+for fruit in fruits:
+    print(fruit)
+
