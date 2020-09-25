790ece53815d0c2a727125f4eafb5b5dfd83d041
adrian-zulnedi <z.adrian@arkth.co.jp>
2020-09-25 15:20:14 +0900

    initial commit

--- AMECOBA_SEMESTER1/ADRIAN_PROJECTS/ADRIAN_PROJECT/PY_PROJECT/pythoncode00.py
+++ AMECOBA_SEMESTER1/ADRIAN_PROJECTS/ADRIAN_PROJECT/PY_PROJECT/pythoncode00.py
@@ -2,13 +2,12 @@
 # 整数を1から100まで順に数え上げながら、* 3 で割り切れるとき、 Fizz を出力* 5 で割り切れるとき、 Buzz を出力* 15 で割り切れるとき、 Fizz Buzz を出力* それ以外のとき、数値をそのまま出力するコードを書け。
 
 
-
 for i in range(100):
     if i % 15 == 0:
         print("FizzBuzz")
     elif i % 5 == 0:
-        print("Buzz")
-    elif i % 3 == 0:
         print("Fizz")
+    elif i % 3 ==0:
+        print("Buzz")
     else:
         print("???")
