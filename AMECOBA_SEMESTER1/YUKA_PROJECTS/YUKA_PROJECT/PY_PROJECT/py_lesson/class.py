803d09b823ae19115afc66ab70183d16bf76bdad
yuka <batonyf@icloud.com>
2020-09-29 00:23:26 +0900

    new

--- AMECOBA_SEMESTER1/YUKA_PROJECTS/YUKA_PROJECT/PY_PROJECT/py_lesson/class.py
+++ AMECOBA_SEMESTER1/YUKA_PROJECTS/YUKA_PROJECT/PY_PROJECT/py_lesson/class.py
@@ -6,6 +6,16 @@
 
 # userに関するデータをまとめるためにクラスを作る
 
-class User:
-    pass
-tom = User()
+class User: #下の物をクラスのインスタンスと言う。
+    pass #中身の無いクラス
+
+cat = User()
+cat.name = 'candy'
+cat.old = 10
+
+dog = User()
+dog.name = 'lucky'
+dog.old = 6
+
+print(cat.name)
+print(dog.name)
