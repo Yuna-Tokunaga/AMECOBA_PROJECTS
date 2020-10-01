43e143459086bf657b7cd4d9f008371be6b2b738
adrian-zulnedi <z.adrian@arkth.co.jp>
2020-10-02 00:34:34 +0900

    commit

--- AMECOBA_SEMESTER1/ADRIAN_PROJECTS/ADRIAN_PROJECT/PY_PROJECT/game.py
+++ AMECOBA_SEMESTER1/ADRIAN_PROJECTS/ADRIAN_PROJECT/PY_PROJECT/game.py
@@ -1,28 +1,28 @@
 import pygame
+
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
-
-pygame.display.set_caption("First Game")
+win  = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Pygameでオブジェクトを動かす")
 
 x = 50
-y = 50
+y  = 50
 
-width = 40
-height = 60
+height = 50
+width = 50
 
 vel = 5
 
+
 run = True
 
-
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(10)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    
 
     keys = pygame.key.get_pressed()
 
@@ -35,8 +35,8 @@ while run:
     if keys [pygame.K_DOWN]:
         y += vel
 
-
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    win.fill((0,0,0))
+    pygame.draw.circle(win, (128,0,250), (x, y), height, width)
     pygame.display.update()
 pygame.quit()
+
